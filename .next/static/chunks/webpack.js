d0a226031e005b621a0340ceca2901acd6040b3a
Halyna Lekhnovska <halynalekhnovska@MacBook-Pro-Halyna.local>
2025-04-11 21:46:18 +0200

    google sheets added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "213e6b6b5ce59491"; }
+/******/ 		__webpack_require__.h = function() { return "e4caf7023dbad3af"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
